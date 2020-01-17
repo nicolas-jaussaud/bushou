355f52b397d96b350bde0c11cfdd4332314d986c
Nicolas <nicolas@teamtangible.com>
2020-01-17 01:37:11 +0100

    Fix loading image issue in final build; Fix permission issue

--- app/components/Heart.js
+++ app/components/Heart.js
@@ -2,15 +2,43 @@ import React, { Component } from 'react';
 import { 
   StyleSheet,
   Image,
-  View,
+  View
 } from 'react-native';
+
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
 
 export default class Heart extends Component {
   
   /**
+   * Constructs a new instance.
+   */
+  constructor(props) {
+    super(props)
+    this.state = { isReady: false };
+  }
+
+  /**
+   * @see https://stackoverflow.com/a/51821784/10491705
+   */
+  async _cacheResourcesAsync() {
+    return Asset.loadAsync([require('../assets/img/heart.png')]);
+  }
+
+  /**
    * Renders the page
    */
   render() {
+    if(!this.state.isReady) {
+      return(
+        <AppLoading
+          startAsync={() => this._cacheResourcesAsync()}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
+
     return(
       <View>
         <Image
